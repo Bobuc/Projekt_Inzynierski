3d6991a8ea607a7fd23a071c366c89b52c4592cd
Bobuc <piotrbobuc97@gmail.com>
2021-04-10 11:21:06 +0200

    Dodanie nowej funkcjonalności
    Dodanie do strony skryptu javascript odpowiedzialnego za pokazanie aktualnej pogody
    ver 0.1
    
    Co-Authored-By: boruc <8341141+boruc@users.noreply.github.com>
    Co-Authored-By: reda198-hypeman <77855080+reda198-hypeman@users.noreply.github.com>

--- api.js
+++ api.js
@@ -1,1 +1,63 @@
-const apiKey = "b344fd2765a2602c4afb314b48ae79f5";
+const apikey = "b344fd2765a2602c4afb314b48ae79f5";
+
+const main = document.getElementById("main");
+const form =  document.getElementById("form");
+const search =  document.getElementById("search");
+
+const url = (city) =>
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
+
+async function getWeatherByLocation(city) {
+    const resp = await fetch(url(city), { origin: "cors" });
+    const respData = await resp.json();
+
+    console.log(respData);
+
+    addWeatherToPage(respData);
+}
+
+
+function addWeatherToPage(data) {
+    const temp = KtoC(data.main.temp);
+    const dateObject = new Date(data.sys.sunrise*1000)
+    const dateObject1 = new Date(data.sys.sunset*1000)
+
+    const weather = document.createElement("div");
+    weather.classList.add("weather");
+
+    weather.innerHTML = `
+        <h1>Pogoda dla ${data.name}</h1>
+        <br/>
+        <h2><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /> ${temp}°C <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /></h2>
+        <small>${(data.weather[0].description).toUpperCase()}</small>
+        
+        <br/>
+        <small>Wilgotność : ${data.main.humidity}%</small>
+        <br/>
+        <small>Prędkość wiatru : ${(data.wind.speed)} km/h</small>
+        <br/>
+        <small>Wschód słońca : ${(dateObject.toLocaleString())}</small>
+        <br/>
+        <small>Zachód słońca : ${(dateObject1.toLocaleString())}</small>
+    `;
+
+    // cleanup
+    main.innerHTML = "";
+
+    main.appendChild(weather);
+}
+
+//kelvin to celsius conversion
+function KtoC(K) {
+    return Math.floor(K - 273.15);
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const city = search.value;
+
+    if (city) {
+        getWeatherByLocation(city);
+    }
+});
